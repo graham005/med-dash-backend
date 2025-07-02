36bb3726fbb4950232ff8739feed86672716a2a4
Enoch Graham <enochgraham003@outlook.com>
2025-07-02 09:20:01 +0300

    Remove Pharmacy and Telemedicine modules, including controllers, services, DTOs, and entities.

--- src/telemedicine/telemedicine.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TelemedicineService } from './telemedicine.service';
-import { TelemedicineController } from './telemedicine.controller';
-
-@Module({
-  controllers: [TelemedicineController],
-  providers: [TelemedicineService],
-})
-export class TelemedicineModule {}
