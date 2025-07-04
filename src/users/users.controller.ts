8dca0fe7b0b660bd98edecce8d53e29ff8d42ea2
Enoch Graham <enochgraham003@outlook.com>
2025-07-04 15:56:55 +0300

    feat: implement role-based access control in user management
    - Added RolesGuard to UsersController to enforce role-based access for user-related actions.
    - Updated create, findAll, findOne, update, and remove methods to restrict access to users with the ADMIN role.
    - Removed public access decorators to enhance security and ensure only authorized users can perform these actions.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Query, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserRole } from 'src/enums';
 import { ApiQuery } from '@nestjs/swagger';
 import { Public } from 'src/auth/decorators/public.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+
 
 @Controller('users')
+@UseGuards(RolesGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @Public()
+  @Roles(UserRole.ADMIN)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  @Public()
+  @Roles(UserRole.ADMIN)
   @ApiQuery({
     name: 'userRole',
     enum: UserRole,
@@ -28,16 +32,19 @@ export class UsersController {
     return this.usersService.findAll(userRole);
   }
 
+  @Roles(UserRole.ADMIN)
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id);
   }
 
+  @Roles(UserRole.ADMIN)
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @Roles(UserRole.ADMIN)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.remove(id);
